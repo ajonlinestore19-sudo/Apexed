6d3c6189dfc95752b7bad8f28890bd22a1056151
Tim Josh <cuasaytimjoshgmail.com>
2026-02-25 23:49:55 +0800

    Commit Footer

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
                 </span>
                 <span className="flex flex-col py-3">
                     <p className="text-sm text-[#929292]">
-                        Stay ahead with Automation Insights get the latest AI
+                        Stay ahead with Automation Insights get the latest AI Automation
                         automation trends and smart solutions delivered straight to your inbox
                     </p>
                 </span>
